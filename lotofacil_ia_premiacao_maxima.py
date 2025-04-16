91d1de1fcbf5d47764943ba23bbfd8865f21e2fc
Diogo-MR <drodrigu29@gmail.com>
2025-04-16 10:05:53 -0300

    Add files via upload

---
+++ lotofacil_ia_premiacao_maxima.py
@@ -0,0 +1,7 @@
+
+# NOVA VERSÃO EM DESENVOLVIMENTO
+# Este arquivo incluirá:
+# - Botão exclusivo para gerar sugestões da IA
+# - Análise de repetição de pares/ímpares em relação ao sorteio anterior
+# - Validações para garantir jogos mais próximos de 15 acertos
+# - Estimativa da quantidade de jogos necessária para atingir 15 dezenas
